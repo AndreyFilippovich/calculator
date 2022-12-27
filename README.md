5d649dfbc9f6ab4d6b826e64d5ebfeea9c4dd5fe
Андрей Филиппович <andrejfilippovic8@gmail.com>
2022-12-27 06:52:42 +0300

    first accept

--- README.md
+++ README.md
@@ -1,9 +1,35 @@
-# calculator
+# Калькулятор
 С графическим интерфейсом GUI
 
-# технологии 
-Python 3.7
-При помощи PySide6 использовался QT Designer (Он находится в venv.lib.site-packages.PySide6.designer.exe)
+### Технологии:
+[![Python](https://img.shields.io/badge/-Python-464646?style=flat-square&logo=Python)](https://www.python.org/)
 
+## Описание проекта
 
+### Калькулятор, который запускается из терминала.
+### При помощи PySide6 использовался QT Designer (Он находится в venv.lib.site-packages.PySide6.designer.exe)
 
+![Внешний вид калькулятора](https://github.com/AndreyFilippovich/calculator/raw/master/image/photo.png)
+
+## Запуск проекта:
+
+#### Склонировать репозиторий:
+> https://github.com/AndreyFilippovich/calculator.git
+
+#### Если ранее не пользовались, то скачать виртуальное окружение:
+> pip install virtualenv
+
+#### Установить виртуальное окружение:
+> python -m venv venv
+
+#### Активировать виртуальное окружение:
+> source venv/scripts/activate
+
+#### Установить зависимости командой:
+> pip install -r requirements.txt
+
+#### Запустить проект командой:
+> python main.py
+
+## Автор проекта
+[AndreyFilippovich](https://github.com/AndreyFilippovich)
--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-import math
 import sys
-from typing import Union, Optional
-from operator import add, sub, mul, truediv
+from operator import add, mul, sub, truediv
+from typing import Optional, Union
 
+from PySide6.QtGui import QFontDatabase  # Для шрифта
 from PySide6.QtWidgets import QApplication, QMainWindow
-from PySide6.QtGui import QFontDatabase # Для шрифта
 
 from design import Ui_MainWindow
-
 
 operations = {
     '+': add,
@@ -19,18 +17,19 @@ operations = {
 error_zero_div = 'Division by zero'
 error_undefined = 'Result is undefined'
 
-default_font_size = 40  
+default_font_size = 40
 default_entry_font_size = 40
+
 
 class Calculator(QMainWindow):
     """Тут располагается дефолтный код для QT приложения.
-     с файлом дизайна
+    с файлом дизайна
     """
     def __init__(self):
         super(Calculator, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.entry_max_len = self.ui.le_entry.maxLength() 
+        self.entry_max_len = self.ui.le_entry.maxLength()
 
         QFontDatabase.addApplicationFont("fonts/Rubik-Regular.ttf")
 
@@ -62,14 +61,17 @@ class Calculator(QMainWindow):
 
     def add_digit(self):
         """Это метод добавляет цифр в поле ввода.
-        Сигнал- это нажатие кнопки, то есть в переменной будет хранится последняя нажатая кнока
-        Чтобы понять, что нажатая кнопка- это цифра, создаем кортеж с именами кнопок цифр
-        Если имя объекта, которое послало сигнал есть в этом кортеже, значит выполняем тот самый метод добавления цифры
+        Сигнал- это нажатие кнопки,
+        то есть в переменной будет хранится последняя нажатая кнока
+        Чтобы понять, что нажатая кнопка- это цифра,
+        создаем кортеж с именами кнопок цифр
+        Если имя объекта, которое послало сигнал есть в этом кортеже,
+        значит выполняем тот самый метод добавления цифры
         """
 
         self.remove_error()
         self.clear_temp_if_equality()
-        btn = self.sender() # QT объект, который посылает сигнал
+        btn = self.sender()  # QT объект, который посылает сигнал
 
         digit_buttons = ('btn_0', 'btn_1', 'btn_2', 'btn_3', 'btn_4',
                          'btn_5', 'btn_6', 'btn_7', 'btn_8', 'btn_9')
@@ -78,8 +80,7 @@ class Calculator(QMainWindow):
             if self.ui.le_entry.text() == '0':
                 self.ui.le_entry.setText(btn.text())
             else:
-                self.ui.le_entry.setText(self.ui.le_entry.text() +btn.text())
-        
+                self.ui.le_entry.setText(self.ui.le_entry.text()+btn.text())
         self.adjust_entry_font_size()
 
     def add_point(self) -> None:
@@ -110,7 +111,7 @@ class Calculator(QMainWindow):
         else:
             self.ui.le_entry.setMaxLength(self.entry_max_len)
             self.adjust_entry_font_size()
-    
+
     def backspace(self) -> None:
         """Метод обризает 1 крайний символ.
         Метод ставит в поле ввода ноль,
@@ -123,7 +124,7 @@ class Calculator(QMainWindow):
             if len(entry) == 2 and '-' in entry:
                 self.ui.le_entry.setText('0')
             else:
-               self.ui.le_entry.setText(entry[:-1]) 
+               self.ui.le_entry.setText(entry[:-1])
         else:
             self.ui.le_entry.setText('0')
         self.adjust_entry_font_size()
@@ -154,15 +155,14 @@ class Calculator(QMainWindow):
             self.adjust_temp_font_size()
 
     @staticmethod
-    def remove_trailing_zeros(num: str) -> str:
+    def remove_trailing_zeros(num: Union[float, int, str]) -> str:
         """Метод для обрезания конечных нулей.
-        Передаем в функцию стринг число, получать- тоже самое.
         Метод делает так, чтобы в конце чисел оставалось максимум .0,
         а не .0000000
         Возвращаем срез строки без двух последних символов, если они равны .0
         Иначе возвращаем просто n."""
         n = str(float(num))
-        return n[:-2] if n[:-2] == '.0' else n
+        return n.replace('.0', '') if n.endswith('.0') else n
 
     def add_temp(self) -> None:
         """Метод для добавления временного выражения.
@@ -190,29 +190,33 @@ class Calculator(QMainWindow):
         return float(entry) if '.' in entry else int(entry)
 
     def get_temp_num(self) -> Union[int, float, None]:
-        """Метод ждя получения числа из временного выражения.
+        """Метод для получения числа из временного выражения.
         Если в лейбле есть текст, то получаем его, разделяем по пробелам
         и берем первый элемент.
         Возвращаем тоже самое, что и в методе получения числа из поля ввода."""
         if self.ui.lbl_temp.text():
             temp = self.ui.lbl_temp.text().strip('.').split()[0]
-            return float(temp) if '.' in temp else int(temp)
+        return float(temp) if '.' in temp else int(temp)
 
-    def get_math_sign(self) -> Optional[str]: # возвращает только строку или ничего
+    # возвращает только строку или ничего
+    def get_math_sign(self) -> Optional[str]:
         """Метод для получения знака из временного выражения.
         Сначала удостоверимся о том, что в лейбле есть текст,
-        затем получаем текст из него, разделяем по пробелам и вытаскиваем последний элемент.
+        затем получаем текст из него,
+        разделяем по пробелам и вытаскиваем последний элемент.
         """
         if self.ui.lbl_temp.text():
             return self.ui.lbl_temp.text().strip('.').split()[-1]
 
     def get_entry_text_width(self) -> int:
         """Вспомогательный метод для получения ширины текста из поля."""
-        return self.ui.le_entry.fontMetrics().boundingRect(self.ui.le_entry.text()).width()
+        return self.ui.le_entry.fontMetrics().boundingRect(
+            self.ui.le_entry.text()).width()
 
     def get_temp_text_width(self) -> int:
         """Вспомогательный метод для получения ширины текста из лейбла."""
-        return self.ui.lbl_temp.fontMetrics().boundingRect(self.ui.lbl_temp.text()).width()
+        return self.ui.lbl_temp.fontMetrics().boundingRect(
+            self.ui.lbl_temp.text()).width()
 
     def calculate(self) -> Optional[str]:
         """Функция вычисления.
@@ -228,9 +232,11 @@ class Calculator(QMainWindow):
         if temp:
             try:
                 result = self.remove_trailing_zeros(
-                    str(operations[self.get_math_sign()](self.get_temp_num(), self.get_entry_num()))
+                    str(operations[self.get_math_sign()](
+                        self.get_temp_num(), self.get_entry_num()))
                 )
-                self.ui.lbl_temp.setText(temp + self.remove_trailing_zeros(entry) + ' =')
+                self.ui.lbl_temp.setText(
+                    temp + self.remove_trailing_zeros(entry) + ' =')
                 self.adjust_temp_font_size()
                 self.ui.le_entry.setText(result)
                 self.adjust_entry_font_size()
@@ -266,7 +272,8 @@ class Calculator(QMainWindow):
                     else:
                         self.ui.lbl_temp.setText(temp[:-2] + f'{btn.text()}')
                 else:
-                    self.ui.lbl_temp.setText(self.calculate() + f'{btn.text()}')
+                    self.ui.lbl_temp.setText(
+                        self.calculate() + f'{btn.text()}')
         except:
             pass
 
@@ -314,24 +321,27 @@ class Calculator(QMainWindow):
     def adjust_entry_font_size(self) -> None:
         """Метод регулирования размера шрифта в поле ввода."""
         font_size = default_entry_font_size
-        while self.get_entry_text_width() > self.ui.le_entry.width() -15:
+        while self.get_entry_text_width() > self.ui.le_entry.width()-15:
             font_size -= 1
-            self.ui.le_entry.setStyleSheet('font-size: ' + str(font_size) + 'pt; border: none;')
+            self.ui.le_entry.setStyleSheet(
+                'font-size: ' + str(font_size) + 'pt; border: none;')
 
         font_size = 1
-        while self.get_entry_text_width() < self.ui.le_entry.width() -60:
+        while self.get_entry_text_width() < self.ui.le_entry.width()-60:
             font_size += 1
 
             if font_size > default_font_size:
                 break
 
-            self.ui.le_entry.setStyleSheet('font-size: ' + str(font_size) + 'pt; border: none;')
+            self.ui.le_entry.setStyleSheet(
+                'font-size: ' + str(font_size) + 'pt; border: none;')
 
     def adjust_temp_font_size(self) -> None:
         font_size = default_font_size
         while self.get_temp_text_width() > self.ui.lbl_temp.width() - 15:
             font_size -= 1
-            self.ui.lbl_temp.setStyleSheet(f'font-size: ' + str(font_size) + 'pt; color: #888;')
+            self.ui.lbl_temp.setStyleSheet(
+                f'font-size: ' + str(font_size) + 'pt; color: #888;')
 
         font_size = 1
         while self.get_temp_text_width() < self.ui.lbl_temp.width() - 60:
@@ -340,13 +350,15 @@ class Calculator(QMainWindow):
             if font_size > default_font_size:
                 break
 
-            self.ui.lbl_temp.setStyleSheet(f'font-size: ' + str(font_size) + 'pt; color: #888;')
+            self.ui.lbl_temp.setStyleSheet(
+                f'font-size: ' + str(font_size) + 'pt; color: #888;')
 
     def resizeEvent(self, event) -> None:
         """Метод меняет размер шрифта при растягивания ширины окна.
         """
         self.adjust_entry_font_size()
         self.adjust_temp_font_size()
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
